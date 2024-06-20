6b3e83d88e637d22da0212fb20491bffe643886e
galuhningrat <147510855+galuhningrat@users.noreply.github.com>
2024-06-20 16:20:45 +0700

    Add files via upload

---
+++ PZEMSoftwareSerial_succeed/PZEMSoftwareSerial_succeed_copy_20240620161947.ino
@@ -0,0 +1,61 @@
+/*
+Copyright (c) 2021 Jakub Mandula
+
+Example of using one PZEM module with Software Serial interface.
+================================================================
+
+If only RX and TX pins are passed to the constructor, software 
+serial interface will be used for communication with the module.
+
+*/
+
+#include <PZEM004Tv30.h>
+#include <SoftwareSerial.h>
+
+#if defined(ESP32)
+    #error "Software Serial is not supported on the ESP32"
+#endif
+
+/* Use software serial for the PZEM
+ * Pin 12 Rx (Connects to the Tx pin on the PZEM)
+ * Pin 13 Tx (Connects to the Rx pin on the PZEM)
+*/
+#if !defined(PZEM_RX_PIN) && !defined(PZEM_TX_PIN)
+#define PZEM_RX_PIN 10
+#define PZEM_TX_PIN 11
+#endif
+
+
+SoftwareSerial pzemSWSerial(PZEM_RX_PIN, PZEM_TX_PIN);
+PZEM004Tv30 pzem(pzemSWSerial);
+
+void setup() {
+    /* Debugging serial */
+    Serial.begin(115200);
+}
+
+void loop() {
+    // Read the data from the sensor
+    float voltage = pzem.voltage();
+    float current = pzem.current();
+    float power = pzem.power();
+    float energy = pzem.energy();
+    float frequency = pzem.frequency();
+    float pf = pzem.pf();
+
+    // Check if the data is valid
+    if (isnan(voltage) || isnan(current) || isnan(power) || isnan(energy) || isnan(frequency) || isnan(pf)) {
+        Serial.println("Error reading sensor data");
+    } else {
+        // Send data in a single line with commas as delimiter
+        Serial.print(voltage); Serial.print(",");
+        Serial.print(current); Serial.print(",");
+        Serial.print(power); Serial.print(",");
+        Serial.print(energy); Serial.print(",");
+        Serial.print(frequency); Serial.print(",");
+        Serial.println(pf);
+    }
+
+    delay(1000);
+}
+
